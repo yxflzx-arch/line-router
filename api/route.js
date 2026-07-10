b5a672a2aaff5923f8908c4fb7ffe2c1b452d642
yxflzx-arch <yxflzx@gmail.com>
2026-07-10 21:45:02 +0800

    Update route.js

--- api/route.js
+++ api/route.js
@@ -1,33 +1,8 @@
-import { Redis } from "@upstash/redis";
+export default function handler(req, res) {
+  res.status(302).setHeader(
+    "Location",
+    "https://lin.ee/wglznlp"
+  );
 
-const redis = Redis.fromEnv();
-
-export default async function handler(req, res) {
-  try {
-    const lines = [
-      "https://lin.ee/wglznlp",
-      "https://lin.ee/O04FjGr"
-    ];
-
-    // Redis 計數
-    const count = await redis.incr("line_count");
-
-    // 輪流分流
-    const target = lines[(count - 1) % lines.length];
-
-    res.statusCode = 302;
-    res.setHeader("Location", target);
-    res.end();
-
-  } catch (error) {
-    console.error(error);
-
-    // Redis 掛掉時，至少還能跳轉
-    res.statusCode = 302;
-    res.setHeader(
-      "Location",
-      "https://lin.ee/wglznlp"
-    );
-    res.end();
-  }
+  res.end();
 }
