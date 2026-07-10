b715a0039ff83e210ee9b3add731a0b8fac4a947
yxflzx-arch <yxflzx@gmail.com>
2026-07-10 21:40:23 +0800

    Update route.js

--- api/route.js
+++ api/route.js
@@ -1,6 +1,9 @@
 import { Redis } from "@upstash/redis";
 
-const redis = Redis.fromEnv();
+const redis = new Redis({
+  url: process.env.KV_REST_API_URL,
+  token: process.env.KV_REST_API_TOKEN,
+});
 
 export default async function handler(req, res) {
 
